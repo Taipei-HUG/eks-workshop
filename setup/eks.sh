8da20b2c8ebf57842056b867630d0e5514d30d1d
owensengoku <owensengoku@gmail.com>
2019-11-05 14:15:38 +0000

    fix all bugs

--- setup/eks.sh
+++ setup/eks.sh
@@ -6,9 +6,6 @@ echo "Creating eks cluster and node group with two t3.large instances ..."
 sed -i "s/@workshop@/${cluster_name}/" eks.yaml
 eksctl create cluster -f eks.yaml
 
-# Get KUBECONFIG then kubectl can work
-aws eks update-kubeconfig --name ${cluster_name}
-
 # Test if kubernate cluster works good
 kubectl get all
 
