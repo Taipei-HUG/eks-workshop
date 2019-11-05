db9614110d5139f76f2ff2201c1c8d8b7263ad96
owensengoku <owensengoku@gmail.com>
2019-11-05 08:31:39 +0000

    fix bug eks.sh

--- setup/eks.sh
+++ setup/eks.sh
@@ -4,7 +4,7 @@ cluster_name=workshop${RANDOM}
 # Create eks cluster using eksctl
 echo "Creating eks cluster and node group with two t3.large instances ..."
 sed -i "s/@workshop@/${cluster_name}/" eks.yaml
-eksctl create cluster -f eks_cluster.yml
+eksctl create cluster -f eks.yaml
 
 # Get KUBECONFIG then kubectl can work
 aws eks update-kubeconfig --name ${cluster_name}
