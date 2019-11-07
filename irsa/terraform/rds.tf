a23ed38edb8fa72ed2d2ec57667bed71ed9e7834
smalltown <smalltown20110306@gmail.com>
2019-11-07 08:12:36 +0800

    spearate data

--- irsa/terraform/rds.tf
+++ irsa/terraform/rds.tf
@@ -1,14 +1,3 @@
-##############################################################
-# Data sources to get VPC, subnets and security group details
-##############################################################
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 ######################
 # DB Account/Password
 ######################
